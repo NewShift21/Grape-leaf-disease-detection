ebd459e95dd97274fb120551d9a782619bcbc2f4
Arnav Tolahunase <141951661+Arnav-Tolahunase@users.noreply.github.com>
2026-02-06 23:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#main file 
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from tensorflow.keras.models import load_model
@@ -102,4 +101,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
